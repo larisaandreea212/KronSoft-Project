528da2344cfb228155dc2a9033b2e8fca1b796af
larisaandreea212 <larisa.sava@student.unitbv.ro>
2026-04-22 11:03:45 +0300

    initial project structure with Spring Boot (Java) backend and Android Compose (Kotlin) frontend

---
+++ frontend/app/src/main/java/com/fmi_unitbv2026/kronsoft_frontend/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.fmi_unitbv2026.kronsoft_frontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fmi_unitbv2026.kronsoft_frontend.ui.theme.KronSoft_frontendTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KronSoft_frontendTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KronSoft_frontendTheme {
+        Greeting("Android")
+    }
+}
